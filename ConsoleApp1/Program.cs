7a6383db2170055693696431b37c81993a015bd9
SoftwareMaker <SoftwareMaker@DESKTOP-GUFEK7S>
2016-11-29 16:11:34 +0200

    in work

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace Ls1
 {
-    class Program
+    class Ls1cmd
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            System.Console.ForegroundColor = System.ConsoleColor.Green;
+            for (int j = 0; j < 500; j++)
+            {
+                for (int i = 0; i < 5000; i++)
+                {
+                    System.Console.Write(i);
+                    System.Threading.Thread.Sleep(1);
+                }
+                //System.Console.Clear();
+            }
+            System.Console.Read();
+
         }
     }
 }
+
