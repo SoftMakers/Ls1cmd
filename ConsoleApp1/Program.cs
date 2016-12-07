b40aa692565c0fe4fdc521e5b9b9bc7146614020
SoftwareMaker <SoftwareMaker@DESKTOP-GUFEK7S>
2016-12-07 16:38:09 +0200

    7.12.16 16:37

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Type testimony of counter with a fractional part here: ");
+        var nt = Console.ReadLine();
+        try
         {
+            Convert.ToDouble(nt);
+            Console.WriteLine("Var was initialize that {0}", nt.GetType());
+            Console.WriteLine("{0:#.###} this is your testimory!", Convert.ToDouble(nt));
+            Console.WriteLine("Price: 61.52 грн\\м.куб");
+            Console.WriteLine("Total price: {0:C}", Convert.ToDouble(nt)*61.52);
+
         }
-    }
+        catch { Console.WriteLine("Type only double!"); }        
+        Console.Read();
+        }
 }
